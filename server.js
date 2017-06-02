a7de11370da91f83bdf008662382f8f6117f0c2a
saravjotsingh <saravjotsaini@gmail.com>
2017-06-02 22:03:06 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const hbs = require('hbs');
+const port = process.env.PORT || 3000;
+
+hbs.registerPartials(__dirname + '/partials');
+
+var app = express();
+app.use(express.static(__dirname + '/partials'));
+app.use(express.static(__dirname + '/views/css'));
+
+app.get('/',(req,res)=>{
+    res.render('mainpage.hbs');
+});
+
+app.get('/about',(req,res)=>{
+    res.render('aboutpage.hbs');
+});
+
+app.get('/contact',(req,res)=>{
+    res.render('contactpage.hbs');
+});
+
+
+app.listen(port,()=>{
+    console.log('server is running');
+})
